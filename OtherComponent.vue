e5c7aaa1ad971b484887baf98bc8ce900ba9cdba
Max <max@zenitht.com>
2019-12-30 20:19:23 -0500

    Create OtherComponent.vue

---
+++ OtherComponent.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col">
+                <div v-if="typeof data.users !== 'undefined' && typeof data.users.data !== 'undefined' && Object.keys(data.users.data).length > 0" class="card-deck">
+                    <div v-for="(user, index) in data.users.data" :key="index" class="card">
+                        <h3 class="text-center p-1 m-0" v-html="user.id"></h3>
+                    </div>
+                </div>
+
+                <h3 v-else-if="typeof data.users !== 'undefined' && typeof data.users.data !== 'undefined' && Object.keys(data.users.data).length === 0" class="text-center">No results found</h3>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'other-component',
+        props: [
+            'pagination_data',
+            'pagination_goto'
+        ],
+        watch: {
+            pagination_goto: {
+                handler: 'getUsers',
+                immediate: true
+            }
+        },
+        methods: {
+            routeChange(to){
+                const t = this;
+
+                t.getUsers();
+            },
+            initComp(){
+                const t = this;
+
+                t.getUsers();
+            },
+            getUsers(page = ''){
+                const   t = this;
+
+                if(typeof page === 'number'){
+                    page = '?page=' + page;
+                }
+
+                axios.get('/users/' + page)
+                    .then(response => {
+                        t.data = response.data;
+                        t.$emit('update:pagination_data', t.data.users);
+                    });
+            }
+        },
+        mounted() {
+            const t = this;
+
+            t.initComp();
+        },
+        data() {
+            return {
+                data: []
+            };
+        }
+    }
+</script>
